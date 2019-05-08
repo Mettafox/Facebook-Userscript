6472a0609a4813fd05b4eac547730a2c068602a2
Mettafox <onehighdegree@gmail.com>
2019-05-08 02:20:56 +0100

    Update facebook_most_recent.user.js

--- facebook_most_recent.user.js
+++ facebook_most_recent.user.js
@@ -1,25 +1,3 @@
-/*  
-    Facebook Auto "Most Recent" Stories
-    This script change the Facebook feed automatically to the most recent publications (stories).
-    As well, this script change the publications order of the groups to most recent publications.
-    
-    This script was published on the Greasy Fork platform (greasyfork.org).
-    Homepage URL: https://greasyfork.org/en/scripts/382099-facebook-auto-most-recent-stories
-    Copyright (C) 2019  Hélder Ferreira
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
 // ==UserScript==
 // @name        Facebook Auto "Most Recent" Stories
 // @version     0.1557115315
