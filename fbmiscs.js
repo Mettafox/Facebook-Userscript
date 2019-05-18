28a9499f2a712612529dc69550ccb992768839bf
Mettafox <onehighdegree@gmail.com>
2019-05-18 02:03:16 +0100

    Update fbmiscs.js

--- fbmiscs.js
+++ fbmiscs.js
@@ -1,17 +1,12 @@
-//a = element to get group ID (ex.: property='al:android:url'), b = attribute that contains the group ID (content), c = group feed option (RECENT_ACTIVITY / CHRONOLOGICAL), d = facebook base URL
-function sortGrp(a, b, c, d){
+// # Arguments: a = element where group ID is located, b = atribute where group ID is, c = type of group disposition (CHRONOLOGICAL or RECENT_ACTIVITY), d = Facebook domain, rs = a resource variable #",
+function sortGrp(a, b, c, d, rs){
   a = a[0].getAttribute(b);
-  var x = a.split("/");
-  return d + "groups/" + x[3] + "/?sorting_setting=" + c.toUpperCase();
+  rs = a.split('/');
+  return d + 'groups/' + rs[3] + '/?sorting_setting=' + c;
 }
 
-// a = URL to split, b = split by
-function splitURL(a, b){
-  a = a.split(b);
-  return a;
-}
-
-function groupSec(a, b){
-  var c = a.toString().replace(/(.?)ref=bookmarks/gi, "");
-  return c + "?sorting_setting=" + b.toUpperCase();
+// # Arguments: a = href attribute, b = type of group disposition (CHRONOLOGICAL or RECENT_ACTIVITY), rs = a resource variable #",
+function groupSec(a, b, rs){
+  rs = a.toString().replace(/(.?)ref=bookmarks/gi, \"\");
+  return rs + \"?sorting_setting=\" + b;
 }
